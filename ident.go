5ddd2ec7233d8f6e05396b50847bf667dccd6cd1
Jay Colson <jay@karma.net>
2022-05-31 12:31:56 +0100

    added options ability for userid

--- ident.go
+++ ident.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net"
+	"os"
 	"runtime/debug"
 )
 
 const (
-	RESPONSE  = " : USERID : UNIX : blackntan\r\n"
+	RESPONSE  = " : USERID : UNIX : "
 	CONN_PORT = "113"
 	CONN_TYPE = "tcp"
 )
 
+var helpFlag = flag.Bool("h", false, "Help")
+var userFlag = flag.String("u", "root", "The userid to respond with in ident request.")
+
 func main() {
+	flag.Parse()
+	if *helpFlag {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	user := *userFlag
 	l, err := net.Listen(CONN_TYPE, ":"+CONN_PORT)
 	if err != nil {
 		log.Fatal(err)
@@ -48,7 +59,7 @@ func main() {
 			if string(buf[i-2]) == "\r" {
 				i -= 1
 			}
-			c.Write(append(buf[:i-1], []byte(RESPONSE)...))
+			c.Write(append(buf[:i-1], []byte(RESPONSE+user+"\r\n")...))
 			c.Close()
 		}(conn)
 	}
